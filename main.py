b2785b6dfb36f5c18db407ff57bfde37f98dae57
Khalid Baowaidhan <Khalidbaowaidhan@gmail.com>
2023-05-13 05:21:40 +0300

    website no images

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("Home.html")
 
 @app.route("/about-us")
 def about_us():
-    return render_template("about_us.html")
+    return render_template("Page-4.html")
 
 @app.route("/our-services")
 def our_service():
-    return render_template("our_services.html")
+    return render_template("Page-2.html")
 
 @app.route("/Privacy")
 def privacy():
-    return render_template("policy.html")
+    return render_template("Page-1.html")
 
 
 if __name__ == "__main__":
